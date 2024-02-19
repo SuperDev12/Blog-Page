208d5484bfd95385a1af0ef8227277a82383f7b0
Dev Rathore <94687428+SuperDev12@users.noreply.github.com>
2024-02-19 10:59:57 +0530

    used filter

--- src/Home.js
+++ src/Home.js
@@ -1,14 +1,18 @@
 import {useState} from 'react';
+import Bloglist from './Bloglist';
 const Home = () => {
-  //let name='mario';
-  const [name, setName] = useState('mario');
+  const [blogs, setBlogs]=useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 }, 
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
+  ]);
+  const handleDelete=(id)=>{
+    const newBlogs=blogs.filter(blog=>blog.id!==id)
+  setBlogs(newBlogs);
+}
     return ( 
         <div className="home">
-            <h2>HomePage</h2>
-            <p>{name}</p>
-            <button onClick={() => {
-                name === 'mario' ? setName('luigi') : setName('mario');
-            }}>Click me</button>
+         <Bloglist blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
         </div>
      );
 }
